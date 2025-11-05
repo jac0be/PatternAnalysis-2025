3c6ddaa86313258e6c136ca28985125524391dd9
jac0be <jac0b@DESKTOP-MD0CHD6.localdomain>
2025-11-05 18:50:44 +1000

    Implemented initial dataset handler.

--- flan-t5/dataset.py
+++ flan-t5/dataset.py
@@ -1,5 +1,20 @@
+# Simple dataset handler
 from datasets import load_dataset
+from torch.utils.data import Dataset
 
-ds = load_dataset("BioLaySumm/BioLaySumm2025-LaymanRRG-opensource-track")
+class BioSummDataset(Dataset):
+    def __init__(self, split="train"):
+        self.ds = load_dataset("BioLaySumm/BioLaySumm2025-LaymanRRG-opensource-track")
 
-print(ds)
+    def __len__(self):
+        return len(self.ds)
+
+    def __getitem__(self, i):
+        # we do not care about the image or source
+        x = self.ds[i]["radiology_report"]
+        y = self.ds[i]["layman_report"]
+        return x, y
+
+
+
+    
