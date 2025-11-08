df166ca4853d9ca3321631044f0cc3c1ba28ee31
jac0be <jacob.edwards1@uqconnect.edu.au>
2025-11-08 22:30:21 +1000

    Fixed repo structure. Also fixed issue in github where two jac0be accounts were attributed to my commits.

---
+++ recognition/dataset.py
@@ -0,0 +1,16 @@
+# Simple dataset handler
+from datasets import load_dataset
+from torch.utils.data import Dataset
+
+class BioSummDataset(Dataset):
+    def __init__(self, split="train"):
+        self.ds = load_dataset("BioLaySumm/BioLaySumm2025-LaymanRRG-opensource-track")[split]
+
+    def __len__(self):
+        return len(self.ds)
+
+    def __getitem__(self, i):
+        # we do not care about the image or source, only text
+        x = self.ds[i]["radiology_report"]
+        y = self.ds[i]["layman_report"]
+        return x, y    
